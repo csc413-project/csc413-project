40eba0858242da60c8f4abe8a6bc1ce8b26e2b74
Scott <ScottCTD@outlook.com>
2024-04-12 22:07:38 -0400

    Updated the script.

--- src/run_main.sh
+++ src/run_main.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-/h/u7/c1/00/cuifuyan/opt/miniconda3/bin/conda init bash
+source /h/u7/c1/00/cuifuyan/opt/miniconda3/etc/profile.d/conda.sh
 
 # Activate Conda environment
 conda activate csc413proj
 
+export MUJOCO_GL="osmesa"
 # Execute Python script
-python main.py
+python src/main.py
